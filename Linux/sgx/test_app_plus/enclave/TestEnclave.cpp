2dc73021faf39367f4896b21459dce1f36183526
Myles Liu <lyx981mike@gmail.com>
2020-05-04 10:51:02 -0700

    video provenance early sgx test in work 15

--- Linux/sgx/test_app_plus/enclave/TestEnclave.cpp
+++ Linux/sgx/test_app_plus/enclave/TestEnclave.cpp
@@ -237,7 +237,7 @@ void t_sgxver_call_apis(void *image_pixels, size_t size_of_image_pixels, int ima
 	// sign_hash(hash_of_contract, len_of_hash, signature, size_of_actual_signature);
     printf("Hello from enclave!\n");
 	pixel* img_pixels = (pixel*) image_pixels;
-	printf("The very first pixel: R: %d; G: %d; B: %d\n", (int)image_pixels[0].r, (int)image_pixels[0].g, (int)image_pixels[0].b);
+	printf("The very first pixel: R: %d; G: %d; B: %d\n", (int)img_pixels[0].r, (int)img_pixels[0].g, (int)img_pixels[0].b);
 
 	/* call the API for verification here */
 	/* FIXME */

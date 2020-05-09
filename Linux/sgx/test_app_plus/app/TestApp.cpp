9f6c0da8369aa555d98500a517d553de001b0ac2
Myles Liu <lyx981mike@gmail.com>
2020-05-08 18:46:51 -0700

    video provenance early sgx test in work 38

--- Linux/sgx/test_app_plus/app/TestApp.cpp
+++ Linux/sgx/test_app_plus/app/TestApp.cpp
@@ -666,7 +666,7 @@ int verification_reply(
     sgx_status_t status = t_sgxver_call_apis(
         global_eid, image_pixels, sizeof(pixel) * image_width * image_height, image_width, image_height, 
         hash_of_original_raw_file, size_of_hoorf, raw_signature, raw_signature_length, 
-        evp_pkey, sizeof(evp_pkey), size_of_actual_pukey, sizeof(int), processed_pixels);
+        evp_pkey, sizeof(EVP_PKEY), size_of_actual_pukey, sizeof(int), processed_pixels);
     if (status != SGX_SUCCESS) {
         printf("Call to t_sgxver_call_apis has failed.\n");
         return 1;    //Test failed
--- Linux/sgx/test_app_plus/enclave/TestEnclave.cpp
+++ Linux/sgx/test_app_plus/enclave/TestEnclave.cpp
@@ -229,7 +229,7 @@ int sign_hash(void *hash_of_contract, size_t len_of_hash, void *signature, void 
 	return 0;
 }
 
-bool verify_hash(char* hash_of_file, unsigned char* signature, size_t size_of_siganture, EVP_KEY* public_key){
+bool verify_hash(char* hash_of_file, unsigned char* signature, size_t size_of_siganture, EVP_PKEY* public_key){
 	// Return true on success; otherwise, return false
 	EVP_MD_CTX *mdctx;
 	const EVP_MD *md;
@@ -283,7 +283,7 @@ void t_sgxver_call_apis(void *image_pixels, size_t size_of_image_pixels, int ima
 	// rsa_key_gen();
 	// sign_hash(hash_of_contract, len_of_hash, signature, size_of_actual_signature);
     printf("Hello from enclave!\n");
-	bool result_of_verification = verify_hash((char*)hash_of_original_image, (unsigned char*)signature, size_of_actual_signature, (EVP_KEY*)public_key);
+	bool result_of_verification = verify_hash((char*)hash_of_original_image, (unsigned char*)signature, size_of_actual_signature, (EVP_PKEY*)public_key);
 	printf("result_of_verification: %d\n", result_of_verification);
 	pixel* img_pixels = (pixel*) image_pixels;
 	printf("The very first pixel: R: %d; G: %d; B: %d\n", (int)img_pixels[0].r, (int)img_pixels[0].g, (int)img_pixels[0].b);

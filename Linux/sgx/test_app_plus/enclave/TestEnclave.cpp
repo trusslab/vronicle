f1aeb2f1899ce795dd704261feb70a8ebc5231d6
Myles Liu <lyx981mike@gmail.com>
2020-05-09 11:58:04 -0700

    video provenance early sgx test in work 90

--- Linux/sgx/test_app_plus/enclave/TestEnclave.cpp
+++ Linux/sgx/test_app_plus/enclave/TestEnclave.cpp
@@ -338,7 +338,7 @@ void t_sgxver_call_apis(void *image_pixels, size_t size_of_image_pixels, int ima
 	printf("(inside enclave)size of raw signature is: %d\n", size_of_actual_signature);
 	printf("(inside enclave)signature: %s\n", (char*)signature);
 
-	bool result_of_verification = verify_hash((char*)hash_of_original_image, size_of_hooi, (unsigned char*)signature, size_of_actual_signature, (EVP_PKEY*)public_key);
+	bool result_of_verification = verify_hash((char*)hash_of_original_image, size_of_hooi, (unsigned char*)signature, size_of_actual_signature, new_key_from_str);
 	printf("result_of_verification: %d\n", result_of_verification);
 	pixel* img_pixels = (pixel*) image_pixels;
 	printf("The very first pixel: R: %d; G: %d; B: %d\n", (int)img_pixels[0].r, (int)img_pixels[0].g, (int)img_pixels[0].b);

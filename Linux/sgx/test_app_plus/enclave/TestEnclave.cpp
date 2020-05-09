883e24a90836bcd61c95d067102b9fd9ab0726fc
Myles Liu <lyx981mike@gmail.com>
2020-05-09 15:53:23 -0700

    video provenance early sgx test in work 110

--- Linux/sgx/test_app_plus/enclave/TestEnclave.cpp
+++ Linux/sgx/test_app_plus/enclave/TestEnclave.cpp
@@ -339,7 +339,7 @@ void t_sgxver_call_apis(void *image_pixels, size_t size_of_image_pixels, int ima
 	// printf("(inside enclave)size of raw signature is: %d\n", size_of_actual_signature);
 	// printf("(inside enclave)signature: %s\n", (char*)signature);
 
-	bool result_of_verification = verify_hash((char*)hash_of_original_image, size_of_hooi, (unsigned char*)signature, size_of_actual_signature, (EVP_PKEY*)public_key);
+	// bool result_of_verification = verify_hash((char*)hash_of_original_image, size_of_hooi, (unsigned char*)signature, size_of_actual_signature, (EVP_PKEY*)public_key);
 	// // printf("(Inside Enclave)result_of_verification: %d\n", result_of_verification);
 	// pixel* img_pixels = (pixel*) image_pixels;
 	// printf("The very first pixel: R: %d; G: %d; B: %d\n", (int)img_pixels[0].r, (int)img_pixels[0].g, (int)img_pixels[0].b);
@@ -347,7 +347,7 @@ void t_sgxver_call_apis(void *image_pixels, size_t size_of_image_pixels, int ima
 	// printf("The very first pixel(After processed by filter): R: %d; G: %d; B: %d\n", (int)((pixel*)processed_pixels)[0].r, (int)((pixel*)processed_pixels)[0].g, (int)((pixel*)processed_pixels)[0].b);
 
 	// EVP_PKEY_free(public_key_from_str);
-	printf("The new key has been cleared, enclave finished running...\n");
+	// printf("The new key has been cleared, enclave finished running...\n");
 
 	/* call the API for verification here */
 	/* FIXME */

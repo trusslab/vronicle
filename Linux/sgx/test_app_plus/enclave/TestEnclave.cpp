681fd8c0bf9db8a910efb9b451ceccd2af627ec7
Myles Liu <lyx981mike@gmail.com>
2020-05-09 15:28:08 -0700

    video provenance early sgx test in work 100

--- Linux/sgx/test_app_plus/enclave/TestEnclave.cpp
+++ Linux/sgx/test_app_plus/enclave/TestEnclave.cpp
@@ -326,28 +326,28 @@ void t_sgxver_call_apis(void *image_pixels, size_t size_of_image_pixels, int ima
 	PEM_read_bio_PUBKEY(bo, &pukey, 0, 0);
 	BIO_free(bo);
 	*/
-    printf("Hello from enclave!\n");
+    // printf("Hello from enclave!\n");
 
-	// Using public_key directly will cause segementation fault
-	// Probably because of incorrect len_of_pubkey (Don't know how to get the correct size)
-	EVP_PKEY* public_key_from_str = unsigned_chars_to_pub_key((unsigned char*)public_key_str, len_of_pukey_str);
-	// print_public_key(public_key_from_str);
+	// // Using public_key directly will cause segementation fault
+	// // Probably because of incorrect len_of_pubkey (Don't know how to get the correct size)
+	// EVP_PKEY* public_key_from_str = unsigned_chars_to_pub_key((unsigned char*)public_key_str, len_of_pukey_str);
+	// // print_public_key(public_key_from_str);
 
-	// sign_hash(hash_of_contract, len_of_hash, signature, size_of_actual_signature);
-	// print_unsigned_chars((unsigned char*)public_key_str, len_of_pukey_str);
+	// // sign_hash(hash_of_contract, len_of_hash, signature, size_of_actual_signature);
+	// // print_unsigned_chars((unsigned char*)public_key_str, len_of_pukey_str);
 
-	// printf("(inside enclave)size of raw signature is: %d\n", size_of_actual_signature);
-	// printf("(inside enclave)signature: %s\n", (char*)signature);
+	// // printf("(inside enclave)size of raw signature is: %d\n", size_of_actual_signature);
+	// // printf("(inside enclave)signature: %s\n", (char*)signature);
 
-	bool result_of_verification = verify_hash((char*)hash_of_original_image, size_of_hooi, (unsigned char*)signature, size_of_actual_signature, public_key_from_str);
-	printf("(Inside Enclave)result_of_verification: %d\n", result_of_verification);
-	pixel* img_pixels = (pixel*) image_pixels;
-	printf("The very first pixel: R: %d; G: %d; B: %d\n", (int)img_pixels[0].r, (int)img_pixels[0].g, (int)img_pixels[0].b);
-	blur(img_pixels, (pixel*)processed_pixels, image_width, image_width * image_height, 9);
-	printf("The very first pixel(After processed by filter): R: %d; G: %d; B: %d\n", (int)((pixel*)processed_pixels)[0].r, (int)((pixel*)processed_pixels)[0].g, (int)((pixel*)processed_pixels)[0].b);
+	// bool result_of_verification = verify_hash((char*)hash_of_original_image, size_of_hooi, (unsigned char*)signature, size_of_actual_signature, public_key_from_str);
+	// printf("(Inside Enclave)result_of_verification: %d\n", result_of_verification);
+	// pixel* img_pixels = (pixel*) image_pixels;
+	// printf("The very first pixel: R: %d; G: %d; B: %d\n", (int)img_pixels[0].r, (int)img_pixels[0].g, (int)img_pixels[0].b);
+	// blur(img_pixels, (pixel*)processed_pixels, image_width, image_width * image_height, 9);
+	// printf("The very first pixel(After processed by filter): R: %d; G: %d; B: %d\n", (int)((pixel*)processed_pixels)[0].r, (int)((pixel*)processed_pixels)[0].g, (int)((pixel*)processed_pixels)[0].b);
 
-	EVP_PKEY_free(public_key_from_str);
-	printf("The new key has been cleared, enclave finished running...\n");
+	// EVP_PKEY_free(public_key_from_str);
+	// printf("The new key has been cleared, enclave finished running...\n");
 
 	/* call the API for verification here */
 	/* FIXME */

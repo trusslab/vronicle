584ce433b2477c6506e4fd6d31082c5f7b61b2c1
Myles Liu <lyx981mike@gmail.com>
2020-05-08 19:43:12 -0700

    video provenance early sgx test in work 49

--- Linux/sgx/test_app_plus/app/TestApp.cpp
+++ Linux/sgx/test_app_plus/app/TestApp.cpp
@@ -848,7 +848,7 @@ int verification_reply(
     sgx_status_t status = t_sgxver_call_apis(
         global_eid, image_pixels, sizeof(pixel) * image_width * image_height, image_width, image_height, 
         hash_of_original_raw_file, size_of_hoorf, raw_signature, raw_signature_length, 
-        evp_pkey, sizeof(struct evp_pkey_st), size_of_actual_pukey, sizeof(int), processed_pixels);
+        evp_pkey, sizeof(struct evp_pkey_st), processed_pixels);
     if (status != SGX_SUCCESS) {
         printf("Call to t_sgxver_call_apis has failed.\n");
         return 1;    //Test failed

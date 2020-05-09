24c8636dec156d88f8cb61c34b0f48b53e8bbe8b
Myles Liu <lyx981mike@gmail.com>
2020-05-09 15:19:10 -0700

    video provenance early sgx test in work 95

--- Linux/sgx/test_app_plus/enclave/TestEnclave.cpp
+++ Linux/sgx/test_app_plus/enclave/TestEnclave.cpp
@@ -339,10 +339,10 @@ void t_sgxver_call_apis(void *image_pixels, size_t size_of_image_pixels, int ima
 
 	bool result_of_verification = verify_hash((char*)hash_of_original_image, size_of_hooi, (unsigned char*)signature, size_of_actual_signature, new_key_from_str);
 	printf("(Inside Enclave)result_of_verification: %d\n", result_of_verification);
-	pixel* img_pixels = (pixel*) image_pixels;
-	printf("The very first pixel: R: %d; G: %d; B: %d\n", (int)img_pixels[0].r, (int)img_pixels[0].g, (int)img_pixels[0].b);
-	blur(img_pixels, (pixel*)processed_pixels, image_width, image_width * image_height, 9);
-	printf("The very first pixel(After processed by filter): R: %d; G: %d; B: %d\n", (int)((pixel*)processed_pixels)[0].r, (int)((pixel*)processed_pixels)[0].g, (int)((pixel*)processed_pixels)[0].b);
+	// pixel* img_pixels = (pixel*) image_pixels;
+	// printf("The very first pixel: R: %d; G: %d; B: %d\n", (int)img_pixels[0].r, (int)img_pixels[0].g, (int)img_pixels[0].b);
+	// blur(img_pixels, (pixel*)processed_pixels, image_width, image_width * image_height, 9);
+	// printf("The very first pixel(After processed by filter): R: %d; G: %d; B: %d\n", (int)((pixel*)processed_pixels)[0].r, (int)((pixel*)processed_pixels)[0].g, (int)((pixel*)processed_pixels)[0].b);
 
 	/* call the API for verification here */
 	/* FIXME */

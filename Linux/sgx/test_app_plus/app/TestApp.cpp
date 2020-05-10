f4a3278ff8172cbe01c3ae6ec115b270752681ba
Myles Liu <lyx981mike@gmail.com>
2020-05-09 20:18:56 -0700

    video provenance early sgx test in work 151

--- Linux/sgx/test_app_plus/app/TestApp.cpp
+++ Linux/sgx/test_app_plus/app/TestApp.cpp
@@ -443,11 +443,14 @@ int save_processed_frame(pixel* processed_pixels, char* frame_id){
 
 size_t calcDecodeLength(const char* b64input) {
   size_t len = strlen(b64input), padding = 0;
+  printf("The len in calc is: %d\n", (int)len);
 
   if (b64input[len-1] == '=' && b64input[len-2] == '=') //last two chars are =
     padding = 2;
   else if (b64input[len-1] == '=') //last char is =
     padding = 1;
+
+  printf("The padding in calc is: %d\n", (int)padding);
   return (len*3)/4 - padding;
 }
 

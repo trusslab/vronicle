436bf7a14e691a26efb9bd4a018fbfbb62b02637
Myles Liu <lyx981mike@gmail.com>
2020-05-11 12:13:02 -0700

    video provenance early sgx test in work 217

--- Linux/sgx/test_app_plus/enclave/TestEnclave.cpp
+++ Linux/sgx/test_app_plus/enclave/TestEnclave.cpp
@@ -449,11 +449,11 @@ void t_sgxver_call_apis(void *image_pixels, size_t size_of_image_pixels, int ima
 	pixel* img_pixels = (pixel*) image_pixels;
 	// printf("The very first pixel: R: %d; G: %d; B: %d\n", (int)img_pixels[0].r, (int)img_pixels[0].g, (int)img_pixels[0].b);
 	// blur(img_pixels, (pixel*)processed_pixels, image_width, image_width * image_height, 9);
-	blur_9(img_pixels, (pixel*)processed_pixels, image_width, image_width * image_height, 1.0 / 25.0);
+	blur_5(img_pixels, (pixel*)processed_pixels, image_width, image_width * image_height, 1.0 / 25.0);
 	// printf("The very first pixel(After processed by filter): R: %d; G: %d; B: %d\n", (int)((pixel*)processed_pixels)[0].r, (int)((pixel*)processed_pixels)[0].g, (int)((pixel*)processed_pixels)[0].b);
 
 	// Prepare for output processed image file str
-	// pixels_to_raw_str((pixel*)processed_pixels, image_width, image_height, (char*)char_array_for_processed_img_sign, size_of_cafpis);
+	pixels_to_raw_str((pixel*)processed_pixels, image_width, image_height, (char*)char_array_for_processed_img_sign, size_of_cafpis);
 
 	// Generate hash of processed image
 	// str_to_hash((char*)char_array_for_processed_img_sign, strlen((char*)char_array_for_processed_img_sign), (char*)hash_of_processed_image);

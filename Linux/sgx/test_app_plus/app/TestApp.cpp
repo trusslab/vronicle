37d5eef7c41121bf49bd614641f4f33fdd9a439a
Myles Liu <lyx981mike@gmail.com>
2020-05-10 19:13:58 -0700

    video provenance early sgx test in work 185

--- Linux/sgx/test_app_plus/app/TestApp.cpp
+++ Linux/sgx/test_app_plus/app/TestApp.cpp
@@ -531,7 +531,7 @@ char *base64_encode(const unsigned char *data,
 
     *output_length = 4 * ((input_length + 2) / 3);
 
-    char *encoded_data = malloc(*output_length);
+    char *encoded_data = (char*)malloc(*output_length);
     if (encoded_data == NULL) return NULL;
 
     for (int i = 0, j = 0; i < input_length;) {

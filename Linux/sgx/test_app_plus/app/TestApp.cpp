2b38e6960f6135c86a8d67e7750d7187e83cd9d3
Myles Liu <lyx981mike@gmail.com>
2020-05-10 18:03:42 -0700

    video provenance early sgx test in work 173

--- Linux/sgx/test_app_plus/app/TestApp.cpp
+++ Linux/sgx/test_app_plus/app/TestApp.cpp
@@ -718,7 +718,11 @@ int save_signature(unsigned char* signature, int len_of_sign, char* frame_id){
 
     char* base64_signature;
 
-    Base64Encode(signature, len_of_sign, &base64_signature);
+    printf("The base64_signature before assigning signauture is: %s\n", base64_signature);
+
+    Base64Encode(signature, sizeof(signature), &base64_signature);
+
+    printf("The base64_signature after assigning signauture of length %d is: %s\n", sizeof(signature), base64_signature);
 
     char* dirname = "data/processed_raw_sign";
     mkdir(dirname, 0777);

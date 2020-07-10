306336e72004e3ee0e9b23db555e40c15b73901a
Myles Liu <lyx981mike@gmail.com>
2020-07-09 17:13:39 -0700

    client side and encoder added

---
+++ client_side/README.md
@@ -0,0 +1,7 @@
+# video_provenance_client
+
+
+## How to take frames from the webcam
+Use run_take_yuyv_frames.sh
+You can check this file for changing the parameters
+
---
+++ client_side/run_take_yuyv_frames.sh
@@ -0,0 +1,15 @@
+# Clean and make
+make clean
+make
+
+# Setup v4l2 cam (Assuming your webcam is /dev/video0)
+v4l2-ctl -d /dev/video0 -c exposure_auto=1
+v4l2-ctl -d /dev/video0 -c brightness=100
+v4l2-ctl -d /dev/video0 -c exposure_absolute=664
+
+# Set variables
+NUM_OF_FRAMES="60"
+SAVE_FILE_NAME="./out_raw_yuv_frames/out.yuv"
+
+# run
+./take_yuyv_frames $NUM_OF_FRAMES $SAVE_FILE_NAME

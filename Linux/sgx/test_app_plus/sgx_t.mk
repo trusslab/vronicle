f033369ef253416ee01a22d4200616abc73fb55e
Myles Liu <lyx981mike@gmail.com>
2020-07-20 12:37:24 -0700

    decoder lib link

--- Linux/sgx/test_app_plus/sgx_t.mk
+++ Linux/sgx/test_app_plus/sgx_t.mk
@@ -69,6 +69,10 @@ $(error Cannot set DEBUG and SGX_PRERELEASE at the same time!!)
 endif
 endif
 
+# Added to link with H264 decoder libraries
+DECODER_LIB=-lh264bsd
+DECODER_LIB_PATH=$(DECODER_DIR)/lib
+
 # Added to build with SgxSSL libraries
 TSETJMP_LIB := -lsgx_tsetjmp
 OPENSSL_LIBRARY_PATH := $(PACKAGE_LIB)/
@@ -124,8 +128,8 @@ Security_Link_Flags := -Wl,-z,noexecstack -Wl,-z,relro -Wl,-z,now -pie
 
 TestEnclave_Link_Flags := $(SGX_COMMON_CFLAGS) -Wl,--no-undefined -nostdlib -nodefaultlibs -nostartfiles \
 	$(Security_Link_Flags) \
-	$(SgxSSL_Link_Libraries) -L$(SGX_LIBRARY_PATH) \
-	-Wl,--whole-archive -l$(Trts_Library_Name) -Wl,--no-whole-archive \
+	$(SgxSSL_Link_Libraries) -L$(SGX_LIBRARY_PATH) -L$(DECODER_LIB_PATH) \
+	-Wl,--whole-archive -l$(Trts_Library_Name) -l$(DECODER_LIB) -Wl,--no-whole-archive \
 	-Wl,--start-group -lsgx_tstdc -lsgx_tcxx -lsgx_tcrypto $(TSETJMP_LIB) -l$(Service_Library_Name) -Wl,--end-group \
 	-Wl,-Bstatic -Wl,-Bsymbolic -Wl,--no-undefined \
 	-Wl,-pie,-eenclave_entry -Wl,--export-dynamic  \

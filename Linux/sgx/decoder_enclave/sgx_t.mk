ae54c2d9d64c125d2fe5b7d775d217d4a4f1a852
Myles Liu <lyx981mike@gmail.com>
2020-07-26 16:52:26 -0700

    decoder lib link fix in_work

--- Linux/sgx/decoder_enclave/sgx_t.mk
+++ Linux/sgx/decoder_enclave/sgx_t.mk
@@ -164,12 +164,10 @@ test: all
 ######## TestEnclave Objects ########
 
 # Added for H264 Decoder
-
+Decoder_C_Objects := $(wildcard $(DECODER_OBJ_DIR)/*.o)
 libh264bsd.a: 
 	@cd $(DECODER_DIR) && mkdir include obj lib && cp src/*.h include/ && cd obj && gcc -c ../src/*.c && cd .. && ar rcs lib/libh264bsd.a obj/*
 	@echo "GEN => H264 Decoder Shared Library Ready..."
-
-Decoder_C_Objects := $(wildcard $(DECODER_OBJ_DIR)/*.o)
 
 # $(ENCLAVE_DIR)/TestEnclave_t.c: libh264bsd.a $(SGX_EDGER8R) $(ENCLAVE_DIR)/TestEnclave.edl
 # 	@cd $(ENCLAVE_DIR) && $(SGX_EDGER8R) --trusted TestEnclave.edl --search-path $(PACKAGE_INC) --search-path $(SGX_SDK_INC)

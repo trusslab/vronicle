15128454eba92078a01ccd9dfcdba79f5ad92f44
Myles Liu <lyx981mike@gmail.com>
2020-07-26 17:03:03 -0700

    decoder lib link fix in_work

--- Linux/sgx/decoder_enclave/sgx_t.mk
+++ Linux/sgx/decoder_enclave/sgx_t.mk
@@ -219,6 +219,7 @@ TestEnclave.so: $(ENCLAVE_DIR)/TestEnclave_t.o $(ENCLAVE_DIR)/ra_tls_options.o $
 	@echo "Cpp Objs => $(TestEnclave_Cpp_Objects)"
 	@echo "C Objs => $(TestEnclave_C_Objects)"
 	@echo "The dir we get decoder c objs -> $(DECODER_OBJ_DIR)"
+	@echo "Decoder C Files => $(Decoder_C_Files)"
 	@echo "Decoder C Objs => $(Decoder_C_Objects)"
 	@echo "All Objs => $^"
 	$(VCXX) $^ -o $@ $(TestEnclave_Link_Flags)

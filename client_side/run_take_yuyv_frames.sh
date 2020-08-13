522e7a60efe0dabd2a8fba7e778d43bd4bc7dd72
Yoshimichi Nakatsuka <y.nakatsuka443@gmail.com>
2020-08-12 18:05:34 -0700

    Clean directory structure

--- client_side/run_take_yuyv_frames.sh
+++ client_side/run_take_yuyv_frames.sh
@@ -2,16 +2,31 @@
 make clean
 make
 
+# Make Encoder
+cd encoder
+./build_x86.sh
+cd ..
+
 # Setup v4l2 cam (Suppose your webcam is video0)
 v4l2-ctl -d /dev/video0 -c exposure_auto=1
 v4l2-ctl -d /dev/video0 -c brightness=100
 v4l2-ctl -d /dev/video0 -c exposure_absolute=664
 
+# Create directory to save output
+mkdir -p output
+
 # Set variables
 NUM_OF_FRAMES="60"
-SAVE_FILE_NAME="./out_raw_yuv_frames/out.720p"
+VIDEO_FILE_NAME="./output/out.720p"
+ENC_VIDEO_FILE_NAME="./output/out.h264"
+SIG_FILE_NAME="./output/out.sig"
 
 # run
-./take_yuyv_frames $NUM_OF_FRAMES $SAVE_FILE_NAME
+./video_capture/take_yuyv_frames $NUM_OF_FRAMES $VIDEO_FILE_NAME
 
-chmod -x,+r $SAVE_FILE_NAME
+# encode
+./encoder/h264enc_x64 $VIDEO_FILE_NAME $ENC_VIDEO_FILE_NAME
+
+# sign encoded video
+
+chmod -x,+r $VIDEO_FILE_NAME

b571a29cc859f3b3d1ca1beb58400effd3e3aa41
Yoshimichi Nakatsuka <y.nakatsuka443@gmail.com>
2020-12-10 17:18:37 +0000

    Remove unneeded files

--- tcp_server/init_server.sh
+++
@@ -1,7 +0,0 @@
-# start tcp server
-./server 10111 &
-
-# start filter enclave
-cd ../filter_blur/sgx/filter_enclave/; sudo ./TestApp ../../../decoder/sgx/video_data/decoder_cert.der ../../../video_data/raw_for_process_sig/sig ../../../video_data/raw_for_process/raw_for_process ../../../video_data/raw_for_process_metadata/meta ../../../video_data/processed_raw_md/meta
-
-# cd ../filter_blur/sgx/filter_enclave/; sudo ./TestApp ../../../decoder/sgx/video_data/decoder_cert.der ../../../video_data/raw_for_process_sig/sig ../../../video_data/raw_for_process/raw_for_process ../../../video_data/raw_for_process_metadata/meta ../../../video_data/processed_raw_md/meta &

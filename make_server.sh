0a426e579432d1bc111dc2f145abea9ffb223139
Yoshimichi Nakatsuka <y.nakatsuka443@gmail.com>
2020-12-10 18:10:51 +0000

    Add command line arguments to make_server and clean_server

--- make_server.sh
+++ make_server.sh
@@ -1,13 +1,31 @@
-HOME_DIR="/var/www/truesign.doc/video_provenance/video_provenance"
+if [ $# -lt 1 ]
+then
+    echo "Usage: ./make_server.sh <path_to_project_directory> <enable_dcap: default=0>"
+    exit 1
+fi
 
-cd $HOME_DIR/decoder/sgx/decoder_enclave/; make
+if [ "$1" != "" ]
+then
+    HOME_DIR=$1
+else
+    echo "Provide path to project directory"
+    exit 1
+fi
 
-cd $HOME_DIR/filter_blur/sgx/filter_enclave/; make
-cd $HOME_DIR/filter_gray/sgx/filter_enclave/; make
-cd $HOME_DIR/filter_white_balance/sgx/filter_enclave/; make
-cd $HOME_DIR/filter_brightness/sgx/filter_enclave/; make
-cd $HOME_DIR/filter_sharpen/sgx/filter_enclave/; make
-cd $HOME_DIR/filter_denoise_easy/sgx/filter_enclave/; make
-cd $HOME_DIR/filter_test_bundle_sharpen_and_blur/sgx/filter_enclave/; make
+ENABLE_DCAP=0
+if [ "$2" != "" ]
+then
+    ENABLE_DCAP=$2
+fi
 
-cd $HOME_DIR/encoder/tee/sgx/encoder_ra/; make
+cd $HOME_DIR/decoder/sgx/decoder_enclave/; make ENABLE_DCAP=$ENABLE_DCAP
+
+cd $HOME_DIR/filter_blur/sgx/filter_enclave/; make ENABLE_DCAP=$ENABLE_DCAP
+cd $HOME_DIR/filter_gray/sgx/filter_enclave/; make ENABLE_DCAP=$ENABLE_DCAP
+cd $HOME_DIR/filter_white_balance/sgx/filter_enclave/; make ENABLE_DCAP=$ENABLE_DCAP
+cd $HOME_DIR/filter_brightness/sgx/filter_enclave/; make ENABLE_DCAP=$ENABLE_DCAP
+cd $HOME_DIR/filter_sharpen/sgx/filter_enclave/; make ENABLE_DCAP=$ENABLE_DCAP
+cd $HOME_DIR/filter_denoise_easy/sgx/filter_enclave/; make ENABLE_DCAP=$ENABLE_DCAP
+cd $HOME_DIR/filter_test_bundle_sharpen_and_blur/sgx/filter_enclave/; make ENABLE_DCAP=$ENABLE_DCAP
+
+cd $HOME_DIR/encoder/tee/sgx/encoder_ra/; make ENABLE_DCAP=$ENABLE_DCAP

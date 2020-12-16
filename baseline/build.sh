f218714d0305fb1c98f2dc17b0f243ffed1d6aec
Yoshimichi Nakatsuka <y.nakatsuka443@gmail.com>
2020-12-15 17:24:22 -0800

    Make build script simpler

--- baseline/build.sh
+++ baseline/build.sh
@@ -2,24 +2,13 @@
 
 HOME_DIR=$PWD
 
-cd $HOME_DIR/client_side; make clean;
-cd $HOME_DIR/scheduler; make clean;
-cd $HOME_DIR/decoder; make clean;
-cd $HOME_DIR/filter_blur; make clean;
-cd $HOME_DIR/filter_brightness; make clean;
-cd $HOME_DIR/filter_denoise_easy; make clean;
-cd $HOME_DIR/filter_gray; make clean;
-cd $HOME_DIR/filter_sharpen; make clean;
-cd $HOME_DIR/filter_white_balance; make clean;
-cd $HOME_DIR/encoder; make clean;
-
-cd $HOME_DIR/client_side; make;
-cd $HOME_DIR/scheduler; make;
-cd $HOME_DIR/decoder; make;
-cd $HOME_DIR/filter_blur; make;
-cd $HOME_DIR/filter_brightness; make;
-cd $HOME_DIR/filter_denoise_easy; make;
-cd $HOME_DIR/filter_gray; make;
-cd $HOME_DIR/filter_sharpen; make;
-cd $HOME_DIR/filter_white_balance; make;
-cd $HOME_DIR/encoder; make;
+cd $HOME_DIR/client_side; make clean; make;
+cd $HOME_DIR/scheduler; make clean; make;
+cd $HOME_DIR/decoder; make clean; make;
+cd $HOME_DIR/filter_blur; make clean; make;
+cd $HOME_DIR/filter_brightness; make clean; make;
+cd $HOME_DIR/filter_denoise_easy; make clean; make;
+cd $HOME_DIR/filter_gray; make clean; make;
+cd $HOME_DIR/filter_sharpen; make clean; make;
+cd $HOME_DIR/filter_white_balance; make clean; make;
+cd $HOME_DIR/encoder; make clean; make;

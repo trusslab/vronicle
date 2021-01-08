c564fa9eb2ec3a331486f25bb311be1f26bd2e0d
Yoshimichi Nakatsuka <y.nakatsuka443@gmail.com>
2021-01-08 21:37:56 +0000

    Add DCAP support for viewer (not tested)

--- viewer/verifier.cpp
+++ viewer/verifier.cpp
@@ -10,14 +10,18 @@
 #include <assert.h>
 #include "ra-challenger.h"
 #include "metadata.h"
+#ifdef ENABLE_DCAP
+#include "sgx_ql_quote.h"
+#include "sgx_dcap_quoteverify.h"
+#endif
 
 Verifier::Verifier(const std::string &_video_file_name,
                    const std::string &_sig_file_name,
-                   const std::string &_ias_cert_file_name,
+                   const std::string &_cert_file_name,
                    const std::string &_md_file_name) {
     video_file_name =  _video_file_name;
     sig_file_name =    _sig_file_name;
-    ias_cert_file_name = _ias_cert_file_name;
+    cert_file_name = _cert_file_name;
     md_file_name = _md_file_name;
 }
 
@@ -55,10 +59,10 @@ void Verifier::Base64Decode(const char* b64message, unsigned char** buffer, size
 void Verifier::verify() {
     FILE* video_file = fopen(video_file_name.c_str(), "rb");
     FILE* sig_file = fopen(sig_file_name.c_str(), "rb");
-    FILE* ias_cert_file = fopen(ias_cert_file_name.c_str(), "r");
+    FILE* cert_file = fopen(cert_file_name.c_str(), "r");
     FILE* md_json_file = fopen(md_file_name.c_str(), "r");
 
-	if (video_file && sig_file && ias_cert_file && md_json_file) {
+	if (video_file && sig_file && cert_file && md_json_file) {
 		// Read video file
         fseek(video_file, 0, SEEK_END);
 		size_t video_size = ftell(video_file);
@@ -88,15 +92,17 @@ void Verifier::verify() {
 		Base64Decode(b64_sig, &sig, &sig_size);
 
 		// Read Encoder certificate
-        fseek(ias_cert_file, 0, SEEK_END);
-        size_t ias_cert_size = ftell(ias_cert_file);
-        fseek(ias_cert_file, 0, SEEK_SET);
-        char *ias_cert = new char [ias_cert_size];
-        fread(ias_cert, 1, ias_cert_size, ias_cert_file);
-		fclose(ias_cert_file);
+        fseek(cert_file, 0, SEEK_END);
+        size_t cert_size = ftell(cert_file);
+        fseek(cert_file, 0, SEEK_SET);
+        char *cert = new char [cert_size];
+        fread(cert, 1, cert_size, cert_file);
+		fclose(cert_file);
+    	const unsigned char* p = (const unsigned char*)cert;
 
+#ifndef ENABLE_DCAP
 		// Verify IAS certificate
-		int ret = verify_sgx_cert_extensions((uint8_t*)ias_cert, (uint32_t)ias_cert_size);
+		int ret = verify_sgx_cert_extensions((uint8_t*)cert, (uint32_t)cert_size);
 		if (ret) {
 			printf("IAS cert verification failed\n");
 			return;
@@ -105,14 +111,89 @@ void Verifier::verify() {
 		// Extract public key from IAS certificate
 		EVP_PKEY* evp_pubkey = EVP_PKEY_new();
 		X509 *crt = X509_new();
- 	    const unsigned char* p = (unsigned char*)ias_cert;
- 	    crt = d2i_X509(NULL, &p, ias_cert_size);
+ 	    crt = d2i_X509(NULL, &p, cert_size);
  	    assert(crt != NULL);
  	    evp_pubkey = X509_get_pubkey(crt);
 		if(!evp_pubkey){
 			printf("Failed to retreive public key\n");
 			return;
 		}
+#else
+    	int ret = 0;
+    	time_t current_time = 0;
+    	uint32_t supplemental_data_size = 0;
+    	uint8_t *p_supplemental_data = NULL;
+    	quote3_error_t dcap_ret = SGX_QL_ERROR_UNEXPECTED;
+    	sgx_ql_qv_result_t quote_verification_result = SGX_QL_QV_RESULT_UNSPECIFIED;
+    	uint32_t collateral_expiration_status = 1;
+
+    	// Extract DCAP evidence from X509 cert
+    	X509* crt = d2i_X509(NULL, &p, cert_size);
+    	assert(crt != NULL);
+    	ecdsa_attestation_evidence_t evidence = {0, };
+    	openssl_ecdsa_extract_x509_extensions(crt, &evidence);
+
+		//call DCAP quote verify library to get supplemental data size
+        //
+        dcap_ret = sgx_qv_get_quote_supplemental_data_size(&supplemental_data_size);
+        if (dcap_ret == SGX_QL_SUCCESS && supplemental_data_size == sizeof(sgx_ql_qv_supplemental_t)) {
+            printf("\tInfo: sgx_qv_get_quote_supplemental_data_size successfully returned.\n");
+            p_supplemental_data = (uint8_t*)malloc(supplemental_data_size);
+        }
+        else {
+            printf("\tError: sgx_qv_get_quote_supplemental_data_size failed: 0x%04x\n", dcap_ret);
+            supplemental_data_size = 0;
+        }
+
+        //set current time. This is only for sample purposes, in production mode a trusted time should be used.
+        //
+        current_time = time(NULL);
+
+
+        //call DCAP quote verify library for quote verification
+        //here you can choose 'trusted' or 'untrusted' quote verification by specifying parameter '&qve_report_info'
+        //if '&qve_report_info' is NOT NULL, this API will call Intel QvE to verify quote
+        //if '&qve_report_info' is NULL, this API will call 'untrusted quote verify lib' to verify quote, this mode doesn't rely on SGX capable system, but the results can not be cryptographically authenticated
+        dcap_ret = sgx_qv_verify_quote(
+            evidence.quote.data(), (uint32_t)evidence.quote.size(),
+            NULL,
+            current_time,
+            &collateral_expiration_status,
+            &quote_verification_result,
+            NULL,
+            supplemental_data_size,
+            p_supplemental_data);
+        if (dcap_ret == SGX_QL_SUCCESS) {
+            printf("\tInfo: App: sgx_qv_verify_quote successfully returned.\n");
+        }
+        else {
+            printf("\tError: App: sgx_qv_verify_quote failed: 0x%04x\n", dcap_ret);
+        }
+
+        //check verification result
+        //
+        switch (quote_verification_result)
+        {
+        case SGX_QL_QV_RESULT_OK:
+        case SGX_QL_QV_RESULT_CONFIG_NEEDED:
+        case SGX_QL_QV_RESULT_OUT_OF_DATE:
+        case SGX_QL_QV_RESULT_OUT_OF_DATE_CONFIG_NEEDED:
+        case SGX_QL_QV_RESULT_SW_HARDENING_NEEDED:
+        case SGX_QL_QV_RESULT_CONFIG_AND_SW_HARDENING_NEEDED:
+			evp_pubkey = X509_get_pubkey(crt);
+			if(!evp_pubkey){
+				printf("Failed to retreive public key\n");
+				return;
+			}
+            break;
+        case SGX_QL_QV_RESULT_INVALID_SIGNATURE:
+        case SGX_QL_QV_RESULT_REVOKED:
+        case SGX_QL_QV_RESULT_UNSPECIFIED:
+        default:
+            printf("\tError: App: Verification completed with Terminal result: %x\n", quote_verification_result);
+            break;
+        }
+#endif
 
 		// Verify signature
 		EVP_MD_CTX *mdctx = NULL;
--- viewer/verifier.h
+++ viewer/verifier.h
@@ -5,7 +5,7 @@ class Verifier {
 public:
     Verifier(const std::string &video_file_name,
              const std::string &sig_file_name,
-             const std::string &ias_cert_file_name,
+             const std::string &cert_file_name,
              const std::string &md_file_name);
     void verify();
 private:
@@ -13,6 +13,6 @@ private:
     void Base64Decode(const char* b64message, unsigned char** buffer, size_t* length);
     std::string video_file_name;
     std::string sig_file_name;
-    std::string ias_cert_file_name;
+    std::string cert_file_name;
     std::string md_file_name;
 };
